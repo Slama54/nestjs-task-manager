6db8e4954fe061d8ff0289ab02604515a35e184e
wael <wael.slama.issat@gmail.com>
2025-02-06 16:56:00 +0100

    Swagger API Documentation and update nest 11

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,14 @@ async function bootstrap() {
     transform: true,
     whitelist: true,
   }));
+    const config = new DocumentBuilder()
+    .setTitle('Task MAnagement')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen( 3000);
 
 }
